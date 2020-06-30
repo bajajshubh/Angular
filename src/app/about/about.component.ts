02c9096f8561cbd83a6ba69eae3984c4215d97bd
Shubham Bajaj <shubhambajaj@jklu.edu.in>
2020-06-30 14:13:06 +0530

    week2

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Leader } from '../shared/leader';
+import { LeaderService } from '../services/leader.service';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +10,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  leaders: Leader[];
+  constructor(private leaderservice: LeaderService,
+     private route: ActivatedRoute,
+     private location: Location) { }
 
   ngOnInit() {
+    let id=this.route.snapshot.params['id'];
+    this.leaders = this.leaderservice.getLeaders();
+  }
+  goBack(): void{
+    this.location.back();
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
 import { AppRoutingModule } from './app-routing/app-routing.module'
 
 @NgModule({
@@ -29,7 +30,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module'
     FooterComponent,
     AboutComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
@@ -44,7 +46,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module'
   ],
   providers: [
     DishService,
-    PromotionService
+    PromotionService,
+    LeaderService
   ],
   bootstrap: [AppComponent]
 })
