f973460490f9dbc37373eb2053c2608c90a95a08
Shubham Bajaj <shubhambajaj@jklu.edu.in>
2020-07-10 17:26:44 +0530

    week 3

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import{ DISHES } from '../shared/dishes';
-
+import { FormBuilder,FormGroup,Validators } from '@angular/forms';
+import { switchMap, switchMapTo } from 'rxjs/operators';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { Comment } from '../shared/comment';
 
 @Component({
   selector: 'app-dishdetail',
@@ -13,21 +15,104 @@ import { Location } from '@angular/common';
 })
 
 export class DishdetailComponent implements OnInit {
-
+  @ViewChild('fform') commentFormDirective;
+  commentForm: FormGroup;
+  comment: Comment;
+  dishIds: string[];
+  prev: string;
+  next: string;
   dish: Dish;
+  dishcopy:Dish;
+
+    formErrors = {
+      author: '' ,
+      rating: 5 ,
+      comment: ''
+    };
+    validationMessages = {
+      'author': {
+        'required': 'Author is required.' ,
+        'minlength': 'Author must be at least 2 characters long.'
+      } ,
+      'comment': {
+        'required': 'comment is required.' ,
+        'minlength': 'comment must be at least 2 characters long.'
+      } ,
+    };
+
 
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,
+    private fb:FormBuilder) { this.createForm();  }
 
-  ngOnInit() {
-    let id = this.route.snapshot.params['id'];
-    this.dishservice.getDish(id)
-    .subscribe((dish) => this.dish = dish);
+    createForm()
+    {
+      this.commentForm=this.fb.group({
+        author:['',[Validators.required,Validators.minLength(2),Validators.maxLength(25)] ],
+        rating:[5],
+        comment:['',Validators.required]
+
+      });
+      this.commentForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+      this.onValueChanged();
+    }
+    onValueChanged(data?:any)
+    {
+      if(!this.commentForm){ return; }
+      const form =this.commentForm;
+      for(const field in this.formErrors)
+      {
+        if(this.formErrors.hasOwnProperty(field))
+        {
+          //clear previous
+          this.formErrors[field]='';
+          const control = form.get(field);
+        if (control && control.dirty && !control.valid)
+        {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+        }
+
+      }
+
+
+    }
+    onSubmit(){
+      this.comment=this.commentForm.value;
+      this.comment.date= new Date().toISOString();
+      this.dishcopy.comments.push(this.comment);
+      this.commentForm.reset({
+        author: '',
+        rating: 5,
+        comment: ''
+      });
+      this.commentFormDirective.resetForm();
+
+    }
+
+    ngOnInit() {
+      this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
+      this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
+      .subscribe(dish => { this.dish = dish; this.dishcopy=dish ;this.setPrevNext(dish.id); });
+    }
+    setPrevNext(dishId: string){
+      const index = this.dishIds.indexOf(dishId);
+      this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
+      this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
+    }
+    goBack():void
+    {
+      this.location.back();
+    }
+
+
+
   }
-
-  goBack(): void {
-    this.location.back();
-  }
-
-}
--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -11,15 +11,18 @@ export class DishService {
 
   constructor() { }
 
-  getDishes(): Observable<Dish[]> {
+    getDishes(): Observable<Dish[]> {
       return of(DISHES).pipe(delay(2000));
     }
 
-    getDish(id: number): Observable<Dish> {
+    getDish(id: string): Observable<Dish> {
       return of(DISHES.filter((dish) => (dish.id === id))[0]).pipe(delay(2000));
     }
 
     getFeaturedDish(): Observable<Dish> {
       return of(DISHES.filter((dish) => dish.featured)[0]).pipe(delay(2000));
     }
+    getDishIds(): Observable<string[] | any> {
+      return of(DISHES.map(dish => dish.id ));
+}
 }
